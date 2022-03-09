1cad7c2c7c54a02937f6ad8c6f8d170c27ea179a
Jan Kuhfeld <jan.kuhfeld@rub.de>
2022-03-09 17:08:08 +0100

    add underline to lsp config

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -50,7 +50,7 @@ end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = true,
-        underline = false,
+        underline = true,
         signs = true,
         update_in_insert = false,
     })
